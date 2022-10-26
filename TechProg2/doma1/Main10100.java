b59ed85aea8042b42dd521034e672c1481f4246c
TobMit <80719513+TobMit@users.noreply.github.com>
2022-10-26 21:07:28 +0200

    Založenie súboru

---
+++ TechProg2/doma1/Main10100.java
@@ -0,0 +1,23 @@
+package TechProg2.doma1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main10100 {
+    public static void main (String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = 0;
+        while(true) {
+            int longestMatch = 0;
+
+            testCase++;
+
+
+            System.out.printf("%2d. Length of longest match: %d\n", testCase, longestMatch);
+        }
+
+    }
+
+}
