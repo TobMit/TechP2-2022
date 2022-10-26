2597268fc2df4145d01fbd67c54648817a80dfcf
TobMit <80719513+TobMit@users.noreply.github.com>
2022-10-26 21:44:46 +0200

    Už test blank hotový

--- TechProg2/doma1/Main10100.java
+++ TechProg2/doma1/Main10100.java
@@ -1,18 +1,34 @@
 package TechProg2.doma1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 public class Main10100 {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int testCase = 0;
-        while(true) {
-            int longestMatch = 0;
-
+        String row1;
+        String row2;
+        while((row1 = bufferedReader.readLine()) != null) {
+            row2= bufferedReader.readLine();
             testCase++;
+            if (row1 == null) {
+                break;
+            }
+            // odstránenie NonAlphanumeric
+            row1 = row1.replaceAll("[^a-zA-Z0-9]", " ");
+            row2 = row2.replaceAll("[^a-zA-Z0-9]", " ");
+
+
+            if (row1.length() == 0 || row2.length() == 0) {
+                System.out.printf("%2d. Blank!\n", testCase);
+                continue;
+            }
+
+            int longestMatch = 0;
 
 
             System.out.printf("%2d. Length of longest match: %d\n", testCase, longestMatch);
