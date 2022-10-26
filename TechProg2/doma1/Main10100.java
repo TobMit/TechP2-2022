28613fcda0eef283c8a8d6fe24a2e43e93372d03
TobMit <80719513+TobMit@users.noreply.github.com>
2022-10-26 21:58:56 +0200

    Rozdelenie riadkov na slová

--- TechProg2/doma1/Main10100.java
+++ TechProg2/doma1/Main10100.java
@@ -3,6 +3,8 @@ package TechProg2.doma1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 public class Main10100 {
@@ -15,17 +17,26 @@ public class Main10100 {
         while((row1 = bufferedReader.readLine()) != null) {
             row2= bufferedReader.readLine();
             testCase++;
-            if (row1 == null) {
-                break;
-            }
+
             // odstránenie NonAlphanumeric
             row1 = row1.replaceAll("[^a-zA-Z0-9]", " ");
             row2 = row2.replaceAll("[^a-zA-Z0-9]", " ");
 
-
             if (row1.length() == 0 || row2.length() == 0) {
                 System.out.printf("%2d. Blank!\n", testCase);
                 continue;
+            }
+
+            ArrayList<String> arrListRow1 = new ArrayList<String>();
+            ArrayList<String> arrListRow2 = new ArrayList<String>();
+            StringTokenizer stringTokenizer1 = new StringTokenizer(row1, " ");
+            while (stringTokenizer1.hasMoreTokens()) {
+                arrListRow1.add(stringTokenizer1.nextToken());
+            }
+
+            StringTokenizer stringTokenizer2 = new StringTokenizer(row2, " ");
+            while (stringTokenizer2.hasMoreTokens()) {
+                arrListRow2.add(stringTokenizer2.nextToken());
             }
 
             int longestMatch = 0;
