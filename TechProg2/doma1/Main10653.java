b2674473900ede7e98b2fa96e0757ebab89803ca
TobMit <80719513+TobMit@users.noreply.github.com>
2022-10-27 20:02:44 +0200

    Zakladné načitavanie hotové

--- TechProg2/doma1/Main10653.java
+++ TechProg2/doma1/Main10653.java
@@ -1,7 +1,61 @@
 package TechProg2.doma1;
 
+import java.util.Scanner;
+
 public class Main10653 {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int nRow = scanner.nextInt();
+            int nColum = scanner.nextInt();
+
+            if (nRow == 0 && nColum == 0){
+                break;
+            }
+
+            // vytvorenie "hracej" plochy, true == bomba
+            boolean[][] land = new boolean[nRow][nColum];
+            int nRowOfBombs = scanner.nextInt();
+            for (int i = 0; i < nRowOfBombs; i++) {
+                int rowNumber = scanner.nextInt();
+                int nBombs = scanner.nextInt();
+                for (int j = 0; j < nBombs; j++) {
+                    land[rowNumber][scanner.nextInt()] = true;
+                }
+            }
+
+            // prvá súradnica je row a potom column
+            Pair<Integer, Integer> startingPoint = new Pair<>(scanner.nextInt(), scanner.nextInt());
+            Pair<Integer, Integer> finshPoint = new Pair<>(scanner.nextInt(), scanner.nextInt());
+
+
+            // bfs časť
+            boolean found = false;
+            //  x y
+            int[][] pohybRobota = new int[][]{{0,1},{1,0}, {0,-1}, {-1,0}};
+            System.out.println();
+        }
+    }
+
+    /**
+     * Pomocná trieda pre prehľadnejšie ukladanie súradníc
+     */
+    private static class Pair<K, D> {
+        private final K right;
+        private final D left;
+
+        public Pair(K pRight, D pLeft) {
+            this.right = pRight;
+            this.left = pLeft;
+        }
+
+        public K getRight() {
+            return right;
+        }
+
+        public D getLeft() {
+            return left;
+        }
     }
 }
+
