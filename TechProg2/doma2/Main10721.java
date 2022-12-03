1a5dff9cc1dfd9413f58ec3e409b980f7688aaa5
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-03 23:57:32 +0100

    Dokomentované

--- TechProg2/doma2/Main10721.java
+++ TechProg2/doma2/Main10721.java
@@ -8,8 +8,11 @@ public class Main10721 {
         int riadok = 0;
         boolean zaporne = false;
         while (scanner.hasNext()) {
+            // sirka BarCode
             int n = scanner.nextInt();
+            // pocet cisel v BarCode
             int k = scanner.nextInt();
+            // sirka jednoho cisla
             int m = scanner.nextInt();
 
             System.out.println(pocitaj(n, k , m));
@@ -21,13 +24,15 @@ public class Main10721 {
         // riadky sú širka BarCode
         // stlpec sú počet prvkov
         // vďaka tomuto môžem napočitavať postupne všetky kombinácie od 1 až po požadovanú veľkosť
+        // v záverečnom cikle sa napočitávajú jednotlivé možnosti pre rôzne širky čisla
+        // najlešpie je si to napísať do tb pre lepšie pochopene (pre budúceho mňa xD)
         long[][] memorization = new long[50][50];
         // nastavím prvý stlpec - na 1 kde to vychýza po širku znaku
         for (int i = 0; i < n && i < m; i++) {
             memorization[i][0] = 1;
         }
 
-        // dynamické programovaine I gess
+        // dynamické programovaine  I gess
         // postupné budovanie pamete pre postupné zvyšovanie počtu znakov
         for (int pocetZnakov = 1; pocetZnakov < k; pocetZnakov++) {
             // postupné zvyšovanie širky barcode kde spočitavam celú širku znaku resp jeho kombinácie
